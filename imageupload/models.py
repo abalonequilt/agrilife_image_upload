8d04ceef15d0c65fb33fa3d771081396f33ad92f
Erwin Hilton <erwinhilton@protonmail.com>
2022-10-13 12:33:16 -0500

    django files

---
+++ imageupload/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images')
+    name = 'imageupload.Image'
+    #verbose_name = 'Image'
+    def __str__(self):
+        return self.title
+
